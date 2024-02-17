83c4643db8dcc2d98dfb00c57b01a371f00a63bd
lakshmanmulchandani <mulchandanilakshman@gmail.com>
2024-02-17 12:15:42 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+
+const app = express();
+
+
+app.use(bodyParser.json({limit: "30mb", extended: "true"}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: "true"}));
+app.use(cors())
+
+
+const temp  = (req, res) => res.send("Hello from my memories api")
+
+app.get("/",temp );
+
+
+
+
+
+
+app.listen(5000,()=>
+{
+    console.log("listening")
+})
